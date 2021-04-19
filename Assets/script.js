cfba26194f44ec603b5360de9935d577aadf43d2
Michael Rao <jmichaelrao@gmail.com>
2021-04-18 19:59:03 -0500

    updates to js for user validation

--- Assets/script.js
+++ Assets/script.js
@@ -1,36 +1,5 @@
-// Assignment Code
-let generateBtn = document.querySelector("#generate");
-
-let lowerCaseCharacters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-let upperCaseCharacters = [
+// Arrays containing all available chars
+let upperChars = [
   "A",
   "B",
   "C",
@@ -58,78 +27,150 @@ let upperCaseCharacters = [
   "Y",
   "Z",
 ];
-
-let specialCharacters = [
-  "~",
-  "`",
-  "!",
-  "@",
+let lowerChars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let specialChars = [
   "#",
   "$",
   "%",
-  "^",
   "&",
-  "*",
   "(",
   ")",
-  "-",
-  "_",
-  "=",
+  "*",
   "+",
-  "[",
-  "]",
-  "{",
-  "}",
-  "\\",
-  "|",
-  ";",
-  ":",
-  "'",
-  '"',
   ",",
-  "<",
+  "-",
   ".",
-  ">",
   "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
   "?",
+  "@",
+  "[",
 ];
+let numericChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let numericCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-function generatePassword() {
-  // Prompt user for password length
-  let lengthChoice = window.prompt(
-    "Choose number of characters in password (min: 8, max: 128)"
+// Function to collect password params from user
+function passwordParams() {
+  let lengthChoice = parseInt(
+    window.prompt(`How many characters should your password contain, boo?`)
   );
-  if (lengthChoice < 8 || lengthChoice > 128) {
-    window.alert("Please choose length between 8 and 128 characters");
+
+  // Statement to ensure user obeys password params
+  if (!lengthChoice || lengthChoice < 8 || lengthChoice > 128) {
+    window.alert(
+      "Not paying attention, boo? The password must be between 8 and 128 characters!"
+    );
+    return;
   }
-  // Prompt user for character types
-  let lowerCaseChoice = window.prompt(
-    "Should the password contain lowercase characters? (Y/N)"
+
+  //prompt user for uppercase characters
+  let upperChoice = window.confirm(
+    "Do you want to include uppercase characters? Cancel = No, OK = Yes"
   );
-  lowerCaseChoice = lowerCaseChoice.toUpperCase;
-  let upperCaseChoice = window.prompt(
-    "Should the password contain uppercase characters? (Y/N)"
+
+  //prompt user for lowercase characters
+  let lowerChoice = window.confirm(
+    "Do you want to include lowercase characters? Cancel = No, OK = Yes"
   );
-  upperCaseChoice = upperCaseChoice.toUpperCase;
-  let numericChoice = window.prompt(
-    "Should the password contain numbers? (Y/N)"
+
+  //prompt user for special characters
+  let specialChoice = window.confirm(
+    "Do you want to include special characters? Cancel = No, OK = Yes"
   );
-  numericChoice = numericChoice.toUpperCase;
-  let specialChoice = window.prompt(
-    "Should the password contain special characters? (Y/N)"
+
+  //prompt user for numeric characters
+  let numericChoice = window.confirm(
+    "Do you want to include numeric characters? Cancel = No, OK = Yes"
   );
-  specialChoice = specialChoice.toUpperCase;
+
+  // Key value pairs for validated user choices
+  let validatedChoices = {
+    length: lengthChoice,
+    upper: upperChoice,
+    lower: lowerChoice,
+    special: specialChoice,
+    numeric: numericChoice,
+  };
+
+  return validatedChoices;
 }
 
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+// Function to generate password based on parameter choices made in previous function
+function generatePassword() {
+  let choices = passwordParams();
 
-  passwordText.value = password;
+  // Set empty array to be filled by random values based on boolean response
+  let emptyPwArray = [];
+
+  // Add values to empty array
+  if (choices.special) {
+    for (i = 0; i < specialChars.length; ++i) {
+      emptyPwArray.push(specialChars[i]);
+    }
+  }
+  if (choices.numeric) {
+    for (i = 0; i < numericChars.length; ++i) {
+      emptyPwArray.push(numericChars[i]);
+    }
+  }
+  if (choices.lower) {
+    for (i = 0; i < lowerChars.length; ++i) {
+      emptyPwArray.push(lowerChars[i]);
+    }
+  }
+  if (choices.upper) {
+    for (i = 0; i < upperChars.length; ++i) {
+      emptyPwArray.push(upperChars[i]);
+    }
+  }
+
+  // Set empty array to be filled by values that make up final password
+  let almostFinal = [];
+
+  for (let i = 0; i < choices.length; ++i) {
+    let randomPicker = Math.floor(
+      Math.random() * Math.floor(emptyPwArray.length)
+    );
+    almostFinal.push(emptyPwArray[randomPicker]);
+  }
+
+  // Remove punctuation from final password in almostFinal array
+  let finalPW = almostFinal.join("");
+
+  // Write password to textarea based on id
+  document.getElementById("password").textContent = finalPW;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Button to kick off prompts
+let generatePasswordButton = document.getElementById("generate");
+generatePasswordButton.addEventListener("click", generatePassword);
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # passwordgen
+
+Built and tested by Michael Rao
+
+A webpage that allows users to generate a password based on a series of prompts asked using a dialog box. A screenshots of the webpage can be found here ./assets/images/Screenshot.png.
+
+# Technologies Used
+
+    - HTML
+    - CSS
+    - Javascript
+
+# Description
+
+This browser application allows users to click a button to generate a password containing a series of random values. Once the Generate button is clicked, the user must answer a series of prompts to define the password parameters. The user first must choose the password length, then once a valid length is entered, the user may choose if the password should contain one or more of the following character types: upper case, lower case, special, and/or numeric. After valid selections are made, the randomly generated password is written to the text box, where the user can highlight and copy for use elsewhere.
+
+# Setup/Installation Requirements
+
+Clone this repository to VS code. Open /Users/mrao/Code/passwordgen/index.html in your browser.
+
+Alternatively, the webpage can be accessed at https://jmrao002.github.io/passwordgen/
+
+# Known Bugs
+
+This webpage is not optimized for mobile
+
+# License
+
+Copyright (c) Michael Rao
+
+# Contact Information
+
+Michael Rao jmichaelrao@gmail.com
